dd294240ef74cf3b761cfa7da1baf3278b7acb36
Anthon_M <xll2005@tut.by>
2020-04-19 18:28:37 +0300

    feat: add database connection

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
+const app = require('./app');
 const { logger } = require('./logger/logger');
 
 process.on('uncaughtException', error => {
@@ -26,9 +29,15 @@ process.on('unhandledRejection', reason => {
   });
 });
 
-const { PORT } = require('./common/config');
-const app = require('./app');
-
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log("we're connected!");
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+});
